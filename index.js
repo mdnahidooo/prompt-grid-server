92e42462d2587b3036874fc6ca785d3d0af2ed5d
mdnahidooo <md.nahidofficial.cse@gmail.com>
2026-06-19 00:51:49 +0600

    Initial setup with jwt setup

---
+++ index.js
@@ -0,0 +1,100 @@
+const dns = require("node:dns");
+dns.setServers(["1.1.1.1", "1.0.0.1"]);
+
+const express = require("express");
+const dontenv = require("dotenv");
+const cors = require("cors");
+const { MongoClient, ServerApiVersion, ObjectId } = require("mongodb");
+const { createRemoteJWKSet, jwtVerify } = require("jose-cjs");
+dontenv.config();
+
+const uri = process.env.MONGODB_URI;
+
+const app = express();
+const PORT = process.env.PORT || 5000;
+
+app.use(
+    cors({
+        credentials: true,
+        origin: [process.env.CLIENT_URL],
+    }),
+);
+app.use(express.json());
+
+const client = new MongoClient(uri, {
+    serverApi: {
+        version: ServerApiVersion.v1,
+        strict: true,
+        deprecationErrors: true,
+    },
+});
+
+const JWKS = createRemoteJWKSet(
+    new URL(`${process.env.CLIENT_URL}/api/auth/jwks`),
+);
+
+const verifyToken = async (req, res, next) => {
+    const authHeader = req.headers.authorization;
+
+    if (!authHeader || !authHeader.startsWith("Bearer")) {
+        return res.status(401).json({ msg: "Unauthorized" });
+    }
+
+
+    const token = authHeader.split(" ")[1];
+
+    if (!token) {
+        return res.status(401).json({ msg: "Unauthorized" });
+    }
+
+    try {
+        const { payload } = await jwtVerify(token, JWKS);
+        req.user = payload;
+
+        next();
+    } catch (error) {
+        console.log(error);
+        return res.status(401).json({ msg: "Unauthorized" });
+    }
+};
+
+
+
+async function run() {
+    try {
+        await client.connect();
+
+
+        const db = client.db("PromptGrid");
+        const userCollection = db.collection("user");     
+
+
+
+
+
+
+
+
+
+
+
+
+        // Send a ping to confirm a successful connection
+        await client.db("admin").command({ ping: 1 });
+        console.log(
+            "Pinged your deployment. You successfully connected to MongoDB!",
+        );
+    } finally {
+        // Ensures that the client will close when you finish/error
+        // await client.close();
+    }
+}
+run().catch(console.dir);
+
+app.get("/", (req, res) => {
+    res.send("Server is running fine!");
+});
+
+app.listen(PORT, () => {
+    console.log(`Server running on port ${PORT}`);
+});
